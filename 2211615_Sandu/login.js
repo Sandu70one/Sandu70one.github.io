f490e4e23206c8ba017447524710cbf99a475fe4
Sandu70one <sanduaruna70one@gmail.com>
2023-03-03 22:21:54 +0530

    rename

--- 2211615_Sandu/login.js
+++ 2211615_Sandu/login.js
@@ -69,7 +69,7 @@ function login(){
     var pass=document.getElementById("password").value;
     for(i=0 ; i<users.length; i++){
         if(user===users[i].username && pass===users[i].password){
-            window.location.href = "./homePage.html";
+            window.location.href = "./index.html";
             return;
         } else{
             console.log("wrong")
